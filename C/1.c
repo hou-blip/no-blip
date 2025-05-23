410801d8cf01103b41f64da426b9ae61ede1b73d
候儆预 <jyjhyjf@qq.com>
2025-05-23 15:47:45 +0800

    新一次修改

--- C/1.c
+++ C/1.c
@@ -2,23 +2,6 @@
 
 int main()
 {
-    const int number = 10;
-    int x;
-    int count[number];
-    int i;
-    for(i = 0;i<=number;i++){
-        count[i] = 0;
-    }
-    scanf("%d", &x);
-    while(x != -1){
-        if(x>=0&&x<=9){
-            count[x] ++;
-        }
-        scanf("%d", &x);
-    }
-    for(i=0;i<=number;i++){
-        printf("%d:%d\n", i, count[i]);
-    }
-
+    printf("Hello world\n");
     return 0;
 }
